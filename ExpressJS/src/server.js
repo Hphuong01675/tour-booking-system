f0cb199dadc4cd5d4ac01ca31944b8ab1e8626ee
Pham Tran Thien Dang <phamtranthiendang308@gmail.com>
2026-05-12 20:23:31 +0700

    feat: add ReactJS and TailwindCSS for UI, use Axios to call API from ExpressJS and use Redux Hook to manager state of Forgot Password (Bai tap 03)

--- ExpressJS/src/server.js
+++ ExpressJS/src/server.js
@@ -1,11 +1,21 @@
 import "dotenv/config"; // gọi hàm config của dotenv để chạy lệnh process.env.PORT
 import express from "express"; // nạp express
 import bodyParser from "body-parser"; // nạp body-parser lấy tham số từ client /user?id=7
+import cors from "cors"; // Nạp thư viện cors
 import viewEngine from "./config/viewEngine"; // nạp viewEngine
 import initWebRoutes from "./routes/auth.routes"; // nạp file web từ Route
 import connectDB from "./config/configdb";
 
 let app = express();
+
+// config cors
+app.use(
+    cors({
+        origin: "http://localhost:5173", // Cho phép ReactJS truy cập
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true, // Cho phép gửi cookie/headers nếu cần
+    }),
+);
 
 //config app
 app.use(bodyParser.json());
