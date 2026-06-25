8e9b8bc4ab559c1e1755b4b7303d616c15ebc524
Lvhai2k5 <lehai332k5@gmail.com>
2026-06-25 17:00:01 +0700

    feat: sync the third from feature/Admin

--- test_fixed.py
+++
@@ -1,7 +0,0 @@
-﻿import re
-with open('fixed.txt', 'r', encoding='utf-8') as f:
-    content = f.read()
-words = set(re.findall(r'[^\x00-\x7F]+', content))
-with open('non_ascii_fixed.txt', 'w', encoding='utf-8') as f:
-    for w in words:
-        f.write(w + '\n')
